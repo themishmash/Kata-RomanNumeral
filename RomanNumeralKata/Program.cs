c8bc195adbc90c87cb7a76947e44ca0a7c2867f2
Michelle <wong.mich.c@gmail.com>
2020-04-06 16:00:28 +1000

    first commit

---
+++ RomanNumeralKata/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RomanNumeralKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
---
+++ RomanNumeralKata/RomanNumeral.cs
@@ -0,0 +1,43 @@
+namespace RomanNumeralKata
+{
+    public class RomanNumeral
+    {
+        public string TranslateIntToRomanNumeral(int number)
+        {
+            if (number == 1)
+            {
+                return "I";
+            }
+
+            if (number == 2)
+            {
+                return "II";
+            }
+
+            if (number == 3)
+            {
+                return "III";
+            }
+
+            if (number == 5)
+            {
+                return "V";
+            }
+
+            if (number == 10)
+            {
+                return "X";
+            }
+
+            return null;
+        }
+    }
+}
+
+//If input is 7, this becomes 7 - 5 = 2. 
+//Then 5 (V). Maybe variable for number of 'I's. Remainder will be the number of 'I's to add. 
+
+//If input is 4. Then it becomes 4-5 = -1. If the answer is -1 --> add 'I' in front of V.
+
+//If input is 9. Then it becomes 9 - 5 = 4. 
+
