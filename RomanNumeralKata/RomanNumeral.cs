129f80757fd48a56348d210e9261cd9fc280211b
Michelle <wong.mich.c@gmail.com>
2020-04-15 11:14:17 +1000

    refactoring trying to make IV and IX tests pass

--- RomanNumeralKata/RomanNumeral.cs
+++ RomanNumeralKata/RomanNumeral.cs
@@ -7,97 +7,136 @@ namespace RomanNumeralKata
 {
     public class RomanNumeral
     {
-        private static Dictionary<string, int> symbolValues = CreateNumeralsAndValues();
-        
+        public Dictionary<string, int>RomanNumeralDictionary 
+        = new Dictionary<string, int>
+        {
+           
+            {"I", 1},
+            {"V", 5},
+            {"X", 10},
+            {"L", 50},
+            {"C", 100},
+            {"D", 500},
+            {"M", 1000}
+           
+        };
         public string TranslateIntToRomanNumeral(int inputNumber)
         {
-            string numeralOutput = "";
+            string romanOutput = "";
             int modulusRemainder;
             
             
             //TODO Tricky thing to do: loop through conditionals and not to hardcode the numbers like 5, 10, etc
-            if (inputNumber + 1 == 5) //symbolValues[5]
+
+           
+            if (inputNumber + 1 == findMatchingRomanNumeral(inputNumber)) //symbolValues[5]
             {
-                return setRomanNumeralForOneLessThanSymbol(inputNumber, numeralOutput);
+                return setRomanNumeralForOneLessThanSymbol(inputNumber);
+                
             }
             
-            if (inputNumber + 1 == 10)
-            {
-                return setRomanNumeralForOneLessThanSymbol(inputNumber, numeralOutput);
-            }
+            
+            
+            // if (inputNumber + 1 == 5) //symbolValues[5]
+            // {
+            //     return setRomanNumeralForOneLessThanSymbol(inputNumber);
+            // }
+            
+            // if (inputNumber + 1 == 10)
+            // {
+            //     return setRomanNumeralForOneLessThanSymbol(inputNumber);
+            // }
            
             //TODO put the modulus remainder items etc into its own function
-            if (inputNumber < 5)
+            if (inputNumber < 5 && inputNumber + 1 != findMatchingRomanNumeral(inputNumber))
             {
                 modulusRemainder = inputNumber % 5;
                 for (int i = 0; i < modulusRemainder; i++)
                 {
-                    numeralOutput += "I";
+                    romanOutput += "I";
                 }
-                return numeralOutput;
+                return romanOutput;
             }
             
             if (inputNumber >= 5 && inputNumber < 10)
             {
-                numeralOutput = "V";
+                romanOutput = "V";
                 modulusRemainder = inputNumber % 5;
                 for (int i = 0; i < modulusRemainder; i++)
                 {
-                    numeralOutput += "I";
+                    romanOutput += "I";
                 }
 
-                return numeralOutput;
+                return romanOutput;
             }
             
             
-            //TODO EASY FUNCTION TO CREATE. Need to reference the key and value again - see setRomanNumeral for guidance
-            if (inputNumber == 10)
-            {
-                return "X";
-            }
+            romanOutput = checkIfInputNumberMatchesRomanNumeral(inputNumber);
 
-            return null;
+            return romanOutput;
         }
 
-        private static Dictionary<string, int>CreateNumeralsAndValues()
-        {
-            Dictionary<string, int> numeralValues = new Dictionary<string, int>();
-            numeralValues.Add("I", 1);
-            numeralValues.Add("V", 5);
-            numeralValues.Add("X", 10);
-            numeralValues.Add("L", 50);
-            numeralValues.Add("C", 100);
-            numeralValues.Add("D", 500);
-            numeralValues.Add("M", 1000);
-            return numeralValues;
-        }
+       
 
-        public string setRomanNumeralForOneLessThanSymbol(int inputNumber, string numeralOutput)
+        public string setRomanNumeralForOneLessThanSymbol(int inputNumber)
         {
-            foreach (KeyValuePair<string, int> dictionaryEntry in symbolValues)
+            foreach (KeyValuePair<string, int> dictionaryEntry in RomanNumeralDictionary)
                 
                 if (inputNumber + 1 == dictionaryEntry.Value)
                 {
-                    numeralOutput = "I" + dictionaryEntry.Key; 
-                    return numeralOutput;
+                    
+                    string romanOutput = "I" + dictionaryEntry.Key; 
+                    return romanOutput;
                 }
 
             return "";
         }
+
+        // public string findMatchingInputNumberForOneLessThanSymbol()
+        // {
+        //     
+        // }
+        
+        
+
+        public string checkIfInputNumberMatchesRomanNumeral(int inputNumber)
+        {
+            foreach (KeyValuePair<string, int> dictionaryEntry in RomanNumeralDictionary)
+                
+                if (inputNumber == dictionaryEntry.Value)
+                {
+                    return dictionaryEntry.Key;
+                }
+            return "";
+        }
+
+        public int findMatchingRomanNumeral(int inputNumber)
+        {
+            foreach (KeyValuePair<string, int> dictionaryEntry in RomanNumeralDictionary)
+                
+                if (inputNumber == dictionaryEntry.Value)
+                {
+                    return dictionaryEntry.Value;
+                }
+            return 0;
+        }
+        
+        
+      
+       
+        
+        
+        
+        
+        
+        //Maybe can delete this later on
         public string findRomanNumeralFromValue(int value)
         {
-            return symbolValues.FirstOrDefault(x => x.Value == value).Key;
+            return RomanNumeralDictionary.FirstOrDefault(x => x.Value == value).Key;
         }
-        // public string findValueFromRomanNumeral(string value)
-        // {
-        // }
+      
     }
 }
 
-//If input is 7, this becomes 7 - 5 = 2. 
-//Then 5 (V). Maybe variable for number of 'I's. Remainder will be the number of 'I's to add. 
 
-//If input is 4. Then it becomes 4-5 = -1. If the answer is -1 --> add 'I' in front of V.
-
-//If input is 9. Then it becomes 9 - 5 = 4. 
 
